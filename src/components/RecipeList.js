3f57e1cdf2b73a6ccfefee7fd347b787639dd086
Julien <jfustier42@gmail.com>
2021-09-14 17:10:18 +0200

    search filters added on recipe list

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -5,6 +5,12 @@ import RecipeCard from './RecipeCard';
 const RecipeList = () => {
 
     const [recipes, setRecipes] = useState(null);
+    const [name, setName] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+    const [numberPerson, setNumberPerson] = useState(1);
+    const [coockTime, setCoockTime] = useState(5);
+    
+    const radios = ['padawan', 'jedi', 'maitre'];
 
     useEffect(() => {
 
@@ -16,10 +22,73 @@ const RecipeList = () => {
 
     return (
         <div className="recipe-list">
+            <div className="sort-container">
+                <div className="input-container">
+                    <h2 className="input-name">Nom</h2>
+                    <input 
+                        type="text" 
+                        className="search-bar"
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+
+                <div className="input-container">
+                    <h2 className="input-name">Difficulté</h2>
+                    <ul className="radio-list">
+                        {radios.map((radio) => {
+                            return (
+                                <li 
+                                    key={radio}
+                                    className="input-radio">
+                                    <input
+                                        type="radio" 
+                                        value={radio} 
+                                        id={radio} 
+                                        checked={radio === difficulty}
+                                        onChange={(e) => setDifficulty(e.target.value)} />
+                                    <label htmlFor={radio}>{radio}</label>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+
+                <div className="input-container">
+                    <h2 className="input-name">Nombre de personne</h2>
+                        <input
+                            value={numberPerson}
+                            type="number" 
+                            min="1" 
+                            className="select-number"
+                            onChange={(e) => setNumberPerson(parseInt(e.target.value))} />
+                </div>
+
+                <div className="input-container">
+                    <h2 className="input-name">Temps de préparation :</h2>
+
+                    <p className="time-coocking">{coockTime} minutes</p>
+
+                    <input
+                        value={coockTime}
+                        type="range" 
+                        min="5" 
+                        max="120"
+                        className="select-range"
+                        onChange={(e) => setCoockTime(parseInt(e.target.value))}
+                    />
+                </div>
+            </div>
+
+            <div className="cancel">
+                {difficulty && (
+                    <h4 onClick={() => setDifficulty("")}>Annuler la recherhe</h4>
+                )}
+            </div>
             
             {recipes ? (
                 <div className="list-container">
-                    {recipes.map((recipe) => (
+                    {recipes
+                    .filter((recipe) => recipe.titre.includes(name) && recipe.niveau.includes(difficulty) && recipe.personnes >= numberPerson && recipe.tempsPreparation >= coockTime)
+                    .map((recipe) => (
                         <RecipeCard recipe={recipe} key={recipe.id} />
                     ))}
                 </div>
