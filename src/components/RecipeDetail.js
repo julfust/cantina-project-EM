32da6e41b1cfcb155d0727466312908b64a3a277
Julien <jfustier42@gmail.com>
2021-09-15 15:59:13 +0200

    detail page added (static version)

---
+++ src/components/RecipeDetail.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+
+const RecipeDetail = () => {
+
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect(() => {
+
+        axios.get(
+            'http://localhost:9000/api/recipe/1'
+        )
+        .then(res => setRecipe(res.data));
+    }, [])
+    
+    return (
+        <>
+            {recipe && (
+                <div className="recipe-detail">
+                    <header>
+                        <h1 className="recipe-title">{recipe.titre}</h1>
+                        <p className="recipe-subtitle"><em>{recipe.description}</em></p>
+                        <hr className="header-division" />
+                    </header>
+
+                    <section className="recipe-informations">
+
+                        <article className="recipe-main">
+                            
+                            <div className="img-container">
+                                <img src={recipe.photo} alt="Plat" />
+                            </div>
+
+                            <div className="step-section">
+
+                                <h2 className="step-section-title">Etapes</h2>
+
+                                <ol className="step-list">
+                                    {recipe.etapes.map((step) => (
+                                        <li className="step">{step}</li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </article>
+
+                        <aside className="recipe-secondary">
+
+                            <div className="button-container">
+                                <Button className="update-button modify-button"><EditOutlined /> Modifier</Button>
+                                <Button className="update-button delete-button"><DeleteOutlined /> Supprimer</Button>
+                            </div>
+
+                            <p className="aside-rubrique">Niveau : {recipe.niveau}</p>
+
+                            <p className="aside-rubrique">Nombre de personnes : {recipe.personnes}</p>
+
+                            <p className="aside-rubrique">Temps de préparation : {recipe.tempsPreparation} min</p>
+
+                            <div className="ingredient-rubrique">
+                                <h3 className="ingredient-rubrique-title">Ingredients :</h3>
+                                
+                                <ul className="ingredient-list">
+                                    
+                                    {recipe.ingredients.map((ingredient) => (
+                                        <li className="ingredient">{ingredient}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </aside>
+                    </section>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default RecipeDetail;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import RecipeDetail from '../components/RecipeDetail';
 
 const Detail = () => {
     return (
         <div>
             <Logo />
             <Navigation />
+            <RecipeDetail />
         </div>
     );
 };
